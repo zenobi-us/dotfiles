036f6dc9cbd92cfa59528af36ef3f8a999f7bc4f
Zeno <airtonix@users.noreply.github.com>
2020-09-14 19:15:23 +0930

    Update setup.linux.user.sh

--- zsh/setup.d/setup.linux.user.sh
+++ zsh/setup.d/setup.linux.user.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
+log() {
+	echo "[SETUP/linux.user] $@"
+}
 
-log "User> Setup"
+log "> Setup"
 
 cd ~;
-log "User> cleaning ~/.z*"
+log "> cleaning ~/.z*"
 rm -rf ~/.z*
 
-log "User> linking zsh files"
+log "> linking zsh files"
 [ -f ${HERE}/.zshrc ] && ln -s ${HERE}/.zshrc ~/
 [ -f ${HERE}/.zprofile ] && ln -s ${HERE}/.zprofile ~/
 [ -f ${HERE}/.zshenv ] && ln -s ${HERE}/.zshenv ~/
 
-log "User> set zsh as shell"
+log "> set zsh as shell"
 chsh -s /bin/zsh $(whoami)
