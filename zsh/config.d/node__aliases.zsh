d4846a4d1922a093c85a0ff41ef5f5be17a37445
Zenobius Jiricek <airtonix@users.noreply.github.com>
2020-10-10 07:14:56 +1030

    wrap nvm stuff in alpine detection

--- zsh/config.d/node__aliases.zsh
+++ zsh/config.d/node__aliases.zsh
@@ -1,1 +1,9 @@
-nvm_get_arch() { nvm_echo "x64-musl"; }
+nvm_get_arch() {
+    case "${OSINFO_PLATFORM}_${OSINFO_NAME}" in
+        linux_alpine)
+            nvm_echo "${OSINFO_PROCESSOR}-musl"
+            ;;
+        *)
+            nvm_echo "${OSINFO_PROCESSOR}";;
+    esac
+}
--- zsh/config.d/node__env.zsh
+++ zsh/config.d/node__env.zsh
@@ -1,2 +1,8 @@
-export NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release
-export NVM_IOJS_ORG_MIRROR=https://example.com
+case "${OSINFO_PLATFORM}_${OSINFO_NAME}" in
+    linux_alpine)
+        export NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release
+        export NVM_IOJS_ORG_MIRROR=https://example.com
+        ;;
+    *)
+        ;;
+esac
