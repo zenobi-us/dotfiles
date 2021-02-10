9caa6616f4f4cabf84cb8aa2649a4ebf5a37f462
Zenobius Jiricek <airtonix@users.noreply.github.com>
2021-02-10 12:32:48 +1030

    update configuration

--- zsh/config.d/global__env.zsh
+++ zsh/config.d/global__env.zsh
@@ -1,4 +1,6 @@
 [ -e "${HOME}/.bin" ] && export PATH=$HOME/.bin:$PATH
+[ -e "${HOME}/.local/bin" ] && export PATH=$HOME/.local/bin:$PATH
+[ -e "${HOME}/.local/share/bin" ] && export PATH=$HOME/.local/share/bin:$PATH
 [ -e "${HOME}/dotfiles/bin" ] && export PATH=$HOME/dotfiles/bin:$PATH
 [ -e "${HOME}/Applications" ] && export PATH=$HOME/Applications:$PATH
 # [ -e "${HOME}/dotfiles/zsh/zfunctions" ] && fpath=( "${HOME}/dotfiles/zsh/zfunctions" $fpath )
--- zsh/config.d/node__env.zsh
+++
@@ -1,8 +0,0 @@
-case "${OSINFO_PLATFORM}_${OSINFO_NAME}" in
-    linux_alpine)
-        export NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release
-        export NVM_IOJS_ORG_MIRROR=https://example.com
-        ;;
-    *)
-        ;;
-esac
