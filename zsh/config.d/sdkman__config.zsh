3388e847ac00615c9e8e9a5bc38182666a5bbe2d
Zenobius Jiricek <airtonix@users.noreply.github.com>
2021-04-07 08:44:00 +0930

    auto install gradle

--- zsh/config.d/sdkman__config.zsh
+++ zsh/config.d/sdkman__config.zsh
@@ -1,6 +1,10 @@
-SDKMAN_HOME="$HOME/.sdkman"
-SDKMAN_BIN="$SDKMAN_HOME/bin"
+export SDKMAN_HOME="$HOME/.sdkman"
+export SDKMAN_BIN="$SDKMAN_HOME/bin"
+export SDKMAN_DIR="$SDKMAN_HOME"
 
 [[ ! -d $SDKMAN_BIN ]] && curl -s "https://get.sdkman.io" | bash
 
-. "$SDKMAN_BIN/sdkman-init.sh"
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+[[ -s "$SDKMAN_BIN/sdkman-init.sh" ]] && source "$SDKMAN_BIN/sdkman-init.sh"
+
+[ ! $(command -v gradle) ] && sdk install gradle
