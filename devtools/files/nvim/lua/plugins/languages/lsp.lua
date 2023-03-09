9519d42b8cb73048a8bf7f528960411a039c0295
Zeno Jiricek <airtonix@users.noreply.github.com>
2023-03-10 09:02:37 +1030

    update lsp with null-ls formatters

--- devtools/files/nvim/lua/plugins/languages/lsp.lua
+++ devtools/files/nvim/lua/plugins/languages/lsp.lua
@@ -1,34 +1,35 @@
 return {
-    "neovim/nvim-lspconfig",
-	dependencies = {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
 
-        { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-		{ "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
+			{ "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+			{ "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
 
-        { "williamboman/mason.nvim" },
-		{ "williamboman/mason-lspconfig.nvim" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-        -- Autocomplete
-		{ "hrsh7th/nvim-cmp" },
-		{ "hrsh7th/cmp-nvim-lua" },
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "hrsh7th/cmp-buffer" },
-		{ "hrsh7th/cmp-path" },
-		{ "hrsh7th/cmp-emoji" },
-		{ "hrsh7th/cmp-nvim-lsp-signature-help" },
-		{ "hrsh7th/cmp-cmdline" },
+			-- Autocomplete
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "hrsh7th/cmp-emoji" },
+			{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+			{ "hrsh7th/cmp-cmdline" },
 
-        -- Signature UI
-		{ "ray-x/lsp_signature.nvim" },
+			-- Signature UI
+			{ "ray-x/lsp_signature.nvim" },
 
-        -- Code Actions
-        { "weilbith/nvim-code-action-menu" },
+			-- Code Actions
+			{ "weilbith/nvim-code-action-menu" },
 
-		-- Snippets
-		{ "L3MON4D3/LuaSnip" },
-		{ "rafamadriz/friendly-snippets" },
-	},
-    opts = {
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+		opts = {
 			-- options for vim.diagnostic.config()
 			diagnostics = {
 				underline = true,
@@ -76,97 +77,188 @@ return {
 				-- ["*"] = function(server, opts) end,
 			},
 		},
-	config = function(lspoptions)
+		config = function(_, lspoptions)
+			--
+			-- AutoFormat
+			--
 
-        --
-        -- AutoFormat
-        --
+			require("lazyvim.plugins.lsp.format").autoformat = lspoptions.autoformat
+			-- setup formatting and keymaps
+			require("lazyvim.util").on_attach(function(client, buffer)
+				require("lazyvim.plugins.lsp.format").on_attach(client, buffer)
+				require("lazyvim.plugins.lsp.keymaps").on_attach(client, buffer)
+			end)
+			-- diagnostics
+			for name, icon in pairs(require("lazyvim.config").icons.diagnostics) do
+				name = "DiagnosticSign" .. name
+				vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+			end
+			vim.diagnostic.config(lspoptions.diagnostics)
 
-        require("lazyvim.plugins.lsp.format").autoformat = lspoptions.autoformat
-        -- setup formatting and keymaps
-        require("lazyvim.util").on_attach(function(client, buffer)
-            require("lazyvim.plugins.lsp.format").on_attach(client, buffer)
-            require("lazyvim.plugins.lsp.keymaps").on_attach(client, buffer)
-        end)
-        -- diagnostics
-        for name, icon in pairs(require("lazyvim.config").icons.diagnostics) do
-            name = "DiagnosticSign" .. name
-            vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
-        end
-        vim.diagnostic.config(lspoptions.diagnostics)
+			--
+			-- Plugin Manager
+			--
 
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = require("plugins.languages.ensure_installed"),
+			})
 
-        --
-        -- Plugin Manager
-        --
+			-- require('legendary').keymaps({
 
-        require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = require("plugins.languages.ensure_installed"),
-		})
+			--     {
 
-        require('legendary').keymaps({
+			--     }
+			-- })
 
-            {
+			--
+			-- Autocomplete
+			--
 
-            }
-        })
+			local cmp = require("cmp")
+			cmp.setup({
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
 
-        --
-        -- Autocomplete
-        --
+				snippet = {
+					expand = function(args)
+						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+					end,
+				},
 
-        local cmp = require("cmp")
-		cmp.setup({
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lsp_signature_help" },
+					{ name = "emoji" },
+				}, {
+					{ name = "buffer" },
+				}),
+
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<Esc>"] = cmp.mapping.abort(),
+
+					-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+			})
+
+			--
+			-- Individual Language Server Setup
+			--
+
+			-- local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+			-- local lsp_attach = function(client, bufnr)
+			-- 	-- Creiate your keybindings here...
+			-- 	-- require
+			-- end
+			-- local lspconfig = require("lspconfig")
+			-- require("mason-lspconfig").setup_handlers({
+			-- 	function(server_name)
+			-- 		lspconfig[server_name].setup({
+			-- 			on_attach = lsp_attach,
+			-- 			capabilities = lsp_capabilities,
+			-- 		})
+			-- 	end,
+			-- })
+
+			local servers = lspoptions.servers
+			local capabilities =
+				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+			local function setup(server)
+				local server_opts = vim.tbl_deep_extend("force", {
+					capabilities = vim.deepcopy(capabilities),
+				}, servers[server] or {})
+
+				if lspoptions.setup[server] then
+					if lspoptions.setup[server](server, server_opts) then
+						return
+					end
+				elseif lspoptions.setup["*"] then
+					if lspoptions.setup["*"](server, server_opts) then
+						return
+					end
+				end
+				require("lspconfig")[server].setup(server_opts)
+			end
+
+			-- temp fix for lspconfig rename
+			-- https://github.com/neovim/nvim-lspconfig/pull/2439
+			local mappings = require("mason-lspconfig.mappings.server")
+			if not mappings.lspconfig_to_package.lua_ls then
+				mappings.lspconfig_to_package.lua_ls = "lua-language-server"
+				mappings.package_to_lspconfig["lua-language-server"] = "lua_ls"
+			end
+
+			local mlsp = require("mason-lspconfig")
+			local available = mlsp.get_available_servers()
+
+			local ensure_installed = {} ---@type string[]
+			for server, server_opts in pairs(servers) do
+				if server_opts then
+					server_opts = server_opts == true and {} or server_opts
+					-- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+					if server_opts.mason == false or not vim.tbl_contains(available, server) then
+						setup(server)
+					else
+						ensure_installed[#ensure_installed + 1] = server
+					end
+				end
+			end
+
+			require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
+			require("mason-lspconfig").setup_handlers({ setup })
+		end,
+	},
+
+	-- formatters
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = { "mason.nvim" },
+		opts = function()
+			local nls = require("null-ls")
+			return {
+				sources = {
+					nls.builtins.formatting.fish_indent,
+					nls.builtins.diagnostics.fish,
+					nls.builtins.formatting.stylua,
+					nls.builtins.formatting.shfmt,
+					nls.builtins.diagnostics.flake8,
+				},
+			}
+		end,
+	},
+
+	-- cmdline tools and lsp servers
+	{
+
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"shfmt",
+				"flake8",
 			},
-
-			snippet = {
-				expand = function(args)
-					-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-				end,
-			},
-
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "nvim_lsp_signature_help" },
-				{ name = "emoji" },
-			}, {
-				{ name = "buffer" },
-			}),
-
-			mapping = cmp.mapping.preset.insert({
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<Esc>"] = cmp.mapping.abort(),
-
-				-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-			}),
-		})
-
-
-        --
-        -- Individual Language Server Setup
-        --
-
-		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local lsp_attach = function(client, bufnr)
-			-- Creiate your keybindings here...
-			-- require
-		end
-		local lspconfig = require("lspconfig")
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					on_attach = lsp_attach,
-					capabilities = lsp_capabilities,
-				})
-			end,
-		})
-	end,
+		},
+		---@param opts MasonSettings | {ensure_installed: string[]}
+		config = function(_, opts)
+			require("mason").setup(opts)
+			local mr = require("mason-registry")
+			for _, tool in ipairs(opts.ensure_installed) do
+				local p = mr.get_package(tool)
+				if not p:is_installed() then
+					p:install()
+				end
+			end
+		end,
+	},
 }
