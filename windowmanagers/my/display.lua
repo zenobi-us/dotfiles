4c153d413ddb932458a4b1910b5fc5329686f0e9
Zeno Jiricek <airtonix@users.noreply.github.com>
2024-04-20 11:32:05 +0930

    Remove awsomewm

--- windowmanagers/my/display.lua
+++
@@ -1,6 +0,0 @@
-local awful = require('awful')
-
-awful.screen.set_auto_dpi_enabled(true)
-
-awful.spawn.with_shell('pidof -x picom || picom')
-awful.spawn.with_shell('systemctl --user start autostart.target')
--- windowmanagers/my/widgets/media-player/init.lua
+++
@@ -1,7 +0,0 @@
-local MediaplayerWidget = require('my.widgets.media-player.MediaplayerWidget')
-local MediaplayerPopup = require('my.widgets.media-player.MediaplayerPopup')
-
-return {
-    Widget = MediaplayerWidget,
-    Popup = MediaplayerPopup
-}
