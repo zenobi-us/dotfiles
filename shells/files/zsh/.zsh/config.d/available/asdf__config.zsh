bd9ea3001e9cb990ce8f5587553c0f00d9da494c
Zeno Jiricek <airtonix@users.noreply.github.com>
2025-07-04 13:00:55 +0930

    tweaks

--- shells/files/zsh/.zsh/config.d/available/asdf__config.zsh
+++ shells/files/zsh/.zsh/config.d/available/asdf__config.zsh
@@ -4,11 +4,6 @@ ASDF_HOME=$HOME/.asdf
 ASDF_BIN=$ASDF_HOME/asdf.sh
 export PATH="$HOME/.asdf/shims:$HOME/.asdf/bin:$PATH"
 
-if [[ ! -f "${ASDF_BIN}" ]]; then
-    sh $DOTFILE_ROOT/lib/setup.bash
-fi
-
-. $ASDF_BIN
 [ -d "$ASDF_HOME/plugins/java/set-java-home.zsh" ] && . $ASDF_HOME/plugins/java/set-java-home.zsh
 
 fpath=($ASDF_HOME/completions $fpath)
