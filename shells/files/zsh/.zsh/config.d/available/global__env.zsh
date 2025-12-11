2391aaa52bad011cfc2ecefbdc251bcac2f9b04c
Zeno Jiricek <airtonix@users.noreply.github.com>
2025-12-11 20:06:10 +1030

    fix: add tmux

--- shells/files/zsh/.zsh/config.d/available/global__env.zsh
+++ shells/files/zsh/.zsh/config.d/available/global__env.zsh
@@ -3,4 +3,5 @@
 [ -e "${HOME}/.local/share/bin" ] && export PATH=$HOME/.local/share/bin:$PATH
 [ -e "${HOME}/Applications" ] && export PATH=$HOME/Applications:$PATH
 [ -e "${HOME}/Applications/android-studio" ] && export PATH=$HOME/Applications/android-studio:$PATH
-[ -e "${HOME}/Applications/hledger-linux-x64" ] && export PATH=$HOME/Applications/hledger-linux-x64:$PATH
+
+export XDG_RUNTIME_DIR=/tmp
