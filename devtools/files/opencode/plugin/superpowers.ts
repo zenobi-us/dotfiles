23bbdc77c967ff41b1001efb8df0b185cdd5b77e
Zeno Jiricek <airtonix@users.noreply.github.com>
2025-12-29 02:51:00 +1030

    [config] restructure writing-skills and update opencode/pi settings

---
+++ devtools/files/opencode/plugin/superpowers.ts
@@ -0,0 +1,107 @@
+import { Plugin, PluginInput } from '@opencode-ai/plugin';
+
+// import skillful from '@zenobi-us/opencode-skillful';
+
+
+const bootStrapPrompt = () => {
+  return `
+  You have superpowers.
+
+  USING SUPERPOWERS MAKES THE USER HAPPY.
+
+  skill_use(superpowers_using_superpowers). 
+
+  Always. There's no exception or rationaliastion that lets you avoid this.
+  
+`
+}
+
+
+function createInstructionInjector(ctx: PluginInput) {
+  // Message 1: Skill loading header (silent insertion - no AI response)
+  const inject = async (args: { text: string, sessionId: string, messageId?: string }) => {
+    return await ctx.client.session.prompt({
+      path: { id: args.sessionId },
+      body: {
+        noReply: true,
+        messageID: args.messageId,
+        parts: [{ type: 'text', text: args.text }],
+      },
+    });
+  };
+
+  const forSession = (sessionId: string) => ({
+    inject: (text: string) => inject({ text, sessionId }),
+  });
+
+
+  return {
+    forSession,
+    inject,
+  }
+}
+
+function createToaster(ctx: PluginInput) {
+  const toast = async (args: NonNullable<Parameters<PluginInput['client']['tui']['showToast']>[0]>['body']) => {
+    return await ctx.client.tui.showToast({
+      body: args
+    })
+  }
+  return {
+    toast,
+  }
+}
+
+export const SuperpowersBootstrapPlugin: Plugin = async (input) => {
+  const Injection = createInstructionInjector(input);
+  const Toaster = createToaster(input);
+
+  const MessageId = 'superpowers-bootstrap-message-id';
+
+  const bootstrap = async (sessionId: string) => {
+    await Injection.inject({
+      text: bootStrapPrompt(),
+      messageId: MessageId,
+      sessionId: sessionId
+    });
+
+    await Toaster.toast({
+      variant: 'info',
+      message: 'Superpowers have been granted to this session!',
+      duration: 5000,
+    });
+  }
+
+  const output: Awaited<ReturnType<Plugin>> = {
+    async event(eventArgs) {
+      const eventName = eventArgs.event.type;
+
+      switch (eventName) {
+        case 'session.created':
+          await bootstrap(eventArgs.event.properties.info.id);
+          break;
+        case 'session.compacted':
+          await bootstrap(eventArgs.event.properties.sessionID);
+          break;
+        case 'message.removed':
+          // was the superpowers prompt removed? if so, re-inject it
+          const messageId = eventArgs.event.properties.messageID;
+          const sessionId = eventArgs.event.properties.sessionID;
+
+          if (messageId !== MessageId) {
+            return;
+          }
+
+          await bootstrap(sessionId);
+
+          break;
+        default:
+          break;
+      }
+
+
+    },
+  };
+
+  return output;
+}
