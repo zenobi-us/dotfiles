6b72019d30c356e2838ec1575a08f111453defc3
Zeno Jiricek <airtonix@users.noreply.github.com>
2026-02-28 02:19:57 +1030

    fix(zsh): fix incorrect printing

--- shells/files/zsh/modules/gnome__env.zsh
+++ shells/files/zsh/modules/gnome__env.zsh
@@ -1,9 +1,9 @@
 PID=`pgrep -n -u $USER gnome-session`
 if [ -n "$PID" ]; then
     export DISPLAY=`awk 'BEGIN{FS="="; RS="\0"}  $1=="DISPLAY" {print $2; exit}' /proc/$PID/environ`
-    echo "DISPLAY set to $DISPLAY"
+    echo "DISPLAY set to $DISPLAY" >&2
 else
-    echo "Could not set DISPLAY"
+    echo "Could not set DISPLAY" >&2
 fi
 unset PID1
 
--- shells/files/zsh/modules/pinentry__config.zsh
+++ shells/files/zsh/modules/pinentry__config.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-pkill gpg-agent
-gpg-agent --pinentry-program=/usr/bin/pinentry-gnome3 --daemon
+pkill gpg-agent 2>/dev/null
+gpg-agent --pinentry-program=/usr/bin/pinentry-gnome3 --daemon 2>/dev/null
