c275a015eae34babd066f8569ed96f45ff4ce055
Zeno Jiricek <airtonix@users.noreply.github.com>
2026-08-06 13:55:48 +0930

    chore(pi): remove obsolete agent extensions

--- devtools/files/pi/agent/extensions/@vanillagreen/pi-agents-tmux/extensions/subagent/names.ts
+++
@@ -1,7 +0,0 @@
-export function safeFileName(value: string): string {
-	return value.replace(/[^\w.-]+/g, "_");
-}
-
-export function shellQuote(value: string): string {
-	return `'${value.replace(/'/g, `'\\''`)}'`;
-}
